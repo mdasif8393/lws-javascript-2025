aa328932832d48ddb18ff3cb8c2b85b9dfd46f64
mdasif8393 <md.asif.8393@gmail.com>
2025-04-15 17:08:45 +0600

    #21 JavaScript Bangla Tutorial | JavaScript Arrays

--- javascript-begineers/21.array.js
+++ javascript-begineers/21.array.js
@@ -1,8 +1,18 @@
+
+////for loop
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 
-let result;
+// let result;
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i])
+// }
 
-for (let i = 0; i < fruits.length; i++) {
-    console.log(fruits[i])
-}
+//// forEach
+// fruits.forEach((fruit) => console.log(fruit))
+
+////push
+// fruits.push("Amra");
+// console.log(fruits);
+
+console.log(fruits instanceof Array);
 
