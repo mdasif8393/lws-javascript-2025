0e9701a278e0a7b71311a0646a7d202015068496
mdasif8393 <md.asif.8393@gmail.com>
2025-04-17 12:44:47 +0600

    #23 JavaScript Bangla Tutorial | JavaScript Array Sort

---
+++ javascript-begineers/22.2.array-search.js
@@ -0,0 +1,34 @@
+// // indexOf
+// const fruits = ["Apple", "Orange", "Apple", "Mango"];
+// const indexOf = fruits.indexOf("Apple", 1)
+// console.log(indexOf);
+
+// // lastIndexOf
+// const fruits = ["Apple", "Orange", "Apple", "Mango"];
+// const lastIndexOf = fruits.lastIndexOf("Apple")
+// console.log(fruits);
+
+// // includes
+// const fruits = ["Apple", "Orange", "Apple", "Mango"];
+// const includes = fruits.includes("Apple")
+// console.log(includes);
+
+// // find
+// const numbers = [4, 9, 16, 25, 29];
+// const find = numbers.find((x) => x > 18)
+// console.log(find);
+
+// //findIndex
+// const numbers = [4, 9, 16, 25, 29];
+// const findIndex = numbers.findIndex((x) => x > 18)
+// console.log(findIndex);
+
+// //findLast()
+// const temp = [27, 28, 30, 40, 42, 35, 30];
+// const findLast = temp.findLast((x) => x > 35)
+// console.log(findLast);
+
+//findLastIndex()
+const temp = [27, 28, 30, 40, 42, 35, 30];
+const findLastIndex = temp.findLastIndex((x) => x > 35)
+console.log(findLastIndex);
---
+++ javascript-begineers/23.sort.js
@@ -0,0 +1,73 @@
+//// sort
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const sort = fruits.sort()
+// console.log(sort);
+
+// // reverse
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.sort()
+// fruits.reverse()
+// console.log(fruits);
+
+// //toSorted
+// const months = ["Jan", "Feb", "Mar", "Apr"];
+// const toSorted = months.toSorted()
+// console.log(toSorted);
+
+// // toReversed()
+// const months = ["Jan", "Feb", "Mar", "Apr"];
+// const toReversed = months.toReversed();
+// console.log(toReversed);
+
+// //Numeric Sort
+// const points = [40, 100, 1, 5, 25, 10];
+
+// points.sort(function (a, b) {
+//     return b - a
+// })
+// console.log(points);
+
+// // sort an array in random order
+// const points = [40, 100, 1, 5, 25, 10];
+// points.sort(function () {
+//     return .5 - Math.random()
+// })
+
+// console.log(points);
+
+// const points = [40, 100, 1, 5, 25, 10];
+// points.sort(function (a, b) {
+//     return b - a
+// })
+// console.log(points[0]);
+
+// //Math,min.apply
+// const points = [40, 100, 1, 5, 25, 10];
+
+// function myArrayMin(arr) {
+//     return Math.min.apply(null, arr)
+// }
+// const min = myArrayMin(points)
+// console.log(min);
+
+// //Math.max.apply
+// const points = [40, 100, 1, 5, 25, 10];
+// function myArrayMax(arr) {
+//     return Math.max.apply(null, points)
+// }
+
+// const max = myArrayMax()
+// console.log(max);
+
+// // Sorting Object Arrays
+// const cars = [
+//     { type: "Volvo", year: 2016 },
+//     { type: "Saab", year: 2001 },
+//     { type: "BMW", year: 2010 }
+// ];
+
+// cars.sort(function (a, b) {
+//     return a.year - b.year
+// })
+// console.log(cars);
+
